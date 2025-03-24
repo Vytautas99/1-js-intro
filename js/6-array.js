40baffabba7343c2809d41fae0326927f3a71afe
Vytautas <wyats45@gmail.com>
2025-03-24 12:16:08 +0200

    update

---
+++ js/6-array.js
@@ -0,0 +1,94 @@
+const students = ['Jonas', 'Petras', 'Petras', 'Ona' ];
+console.log(students);
+
+
+const luckynumbers = [9,8,9,8,9,8];
+const firstNumbers = luckynumbers[0];
+
+console.log(firstNumbers);
+
+const empty = [];
+console.log(empty);
+
+console.log(empty.length);
+console.log(luckynumbers.length);
+console.log(students.length);
+
+const numbers = [10,2,8,4,6];
+
+const totalSum1 = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+console.log(totalSum1);
+
+let totalSum2 = 0;
+totalSum2 = totalSum2 + numbers[0]
+totalSum2 = totalSum2 + numbers[0];
+totalSum2 = totalSum2 + numbers[0];
+totalSum2 = totalSum2 + numbers[0];
+totalSum2 = totalSum2 + numbers[0];
+totalSum2 = totalSum2 + numbers[0];
+
+let totalSum3 = 0;
+totalSum3 += numbers
+
+
+
+
+
+
+
+const vardai = ['a', 'b' , 'c']
+
+const dictionary = ['pomidoras', 'agurkas', 'bulve', 'kefyras', 'druska', 'svogunu laiskai', 'lasiniai']
+
+//patiekalui reikes a, b , c, d. 
+
+const sentence1 = 'patiekalui reikes: ' + dictionary[0] + ", " + dictionary[1] + ', ' + dictionary[2] + ', ' + dictionary[3] + ', ' + dictionary[4] + ', ' + dictionary[5] + ', ' + dictionary[6] + '. '
+
+console.log(sentence1);
+
+
+let sentence2 = 'patiekalui reikes: ';
+sentence2 = sentence2 + dictionary[0];
+sentence2 = sentence2 + ', ';
+sentence2 = sentence2 + dictionary[1];
+sentence2 = sentence2 + ', ';
+sentence2 = sentence2 + dictionary[2];
+sentence2 = sentence2 + ', ';
+sentence2 = sentence2 + dictionary[3];
+sentence2 = sentence2 + ', ';
+sentence2 = sentence2 + dictionary[4];
+sentence2 = sentence2 + ', ';
+sentence2 = sentence2 + dictionary[5];
+sentence2 = sentence2 + ', ';
+sentence2 = sentence2 + dictionary[6];
+sentence2 = sentence2 + '.';
+console.log(sentence2);
+
+
+
+let sentence3 = 'patiekalui reikes: ';
+sentence3 += sentence3 + dictionary[0];
+sentence3 += sentence3 + ', ';
+sentence3 += sentence3 + dictionary[1];
+sentence3 += sentence3 + ', ';
+sentence3 += sentence3 + dictionary[3];
+sentence3 += sentence3 + ', ';
+sentence3 += sentence3 + dictionary[3];
+sentence3 += sentence3 + ', ';
+sentence3 += sentence3 + dictionary[4];
+sentence3 += sentence3 + ', ';
+sentence3 += sentence3 + dictionary[5];
+sentence3 += sentence3 + ', ';
+sentence3 += sentence3 + dictionary[6];
+sentence3 += sentence3 + '.';
+console.log(sentence3);
+
+sentence3 +=  dictionary[0] + ', ';
+
+//let index = index + 1;
+let index = 0;
+sentence += dictionary[index] + ', ';
+index += 1;
+
+
+console.log(sentence3);
